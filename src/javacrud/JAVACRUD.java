069245b4b9b9aa2facaa21349b93727c2b48c2ca
syahrulr16 <sachrul2810@gmail.com>
2021-08-17 23:19:47 +0700

    CRUD GUI

---
+++ src/javacrud/JAVACRUD.java
@@ -0,0 +1,11 @@
+package javacrud;
+
+
+public class JAVACRUD {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/koneksi/Koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author user
+ */
+public class Koneksi {
+    Connection koneksi;
+    
+    public static Connection Koneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/tutorialjava", "root", "");
+            
+            return koneksi;
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            
+            return null;
+        }
+    }
+}
